fd5fce5480d91ab7f378a70e96a0f7e728312777
jorgefortunatof <jorgefortunatof@gmail.com>
2021-06-01 00:27:14 -0300

    Added server-json lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-  return (
-    <>
-      <h1>INDEX</h1>
-    </>
-  );
+//SPA - useEffect
+//SSR - getServerSideProps
+//SSG - getStaticProps
+
+export default function Home({ episodes }) {
+	console.log({ episodes });
+	return (
+		<>
+			<h1>INDEX</h1>
+		</>
+	);
+}
+
+export async function getStaticProps() {
+	const response = await fetch("http://localhost:3333/episodes");
+	const data = await response.json();
+
+	return {
+		props: {
+			episodes: data,
+		},
+		revalidate: 60 * 60 * 6,
+	};
 }
